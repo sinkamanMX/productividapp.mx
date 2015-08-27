a68fae116bb8bbee854fffd63f284981c08a44fc
Enrique Peña Gonzalez <er.penagonzalez@gmail.com>
2015-08-26 19:04:46 -0500

    Update Report
    Update Report

--- public/js/reports/jsReportInfo.js
+++ public/js/reports/jsReportInfo.js
@@ -4,7 +4,58 @@ var bounds;
 var markers = [];
 
 $( document ).ready(function() {    
-   initMapToDraw()
+   initMapToDraw();
+
+  $('.popup-gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+      titleSrc: function(item) {
+        return item.el.attr('title');
+      }
+    },
+    zoom: {
+      enabled: true,
+      duration: 300 // don't foget to change the duration also in CSS
+    }
+  });
+
+  $('.thumbnails').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    closeBtnInside: false,
+    fixedContentPos: true,
+    mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+    image: {
+      verticalFit: true
+    },
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    },    
+    zoom: {
+      enabled: true,
+      duration: 300 // don't foget to change the duration also in CSS
+    }
+  });
+
+    var iHeight = $("#tableResult").height();
+
+    $('#rowGallery').slimscroll({
+        position: 'right',
+        size: '4px',
+        color: themeprimary,
+        height: iHeight,
+    });   
 });  
 
 function initMapToDraw(){
